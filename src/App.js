7d7967a81ae2aadcd14c6bf3ea143462cbeab0a4
Philedo <seahsongli@gmail.com>
2023-02-13 20:08:07 +0800

    Add forms for personal details, experience, education

--- src/App.js
+++ src/App.js
@@ -1,25 +1,121 @@
-import logo from './logo.svg';
+import React, {Component} from "react";
 import './App.css';
+import Particulars from './components/Particulars';
+import uniqid from "uniqid"
+class App extends Component {
+  constructor(props){
+    super(props)
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    this.state = {
+      name: "",
+      title: "",
+      phone: "",
+      email: "",
+      location: "",
+      description: "",
+      workExperience: [],
+      education: [],
+      id:uniqid()
+
+  }
+  }
+
+  handleChange = (e) =>{
+      const value = e.target.value;
+      this.setState({
+        ...this.state,
+        [e.target.name]:value
+      })
+  
+  }
+
+  
+
+  onSubmit = (e) =>{
+    e.preventDefault();
+    this.setState({
+      firstName: "",
+      lastName: "",
+      particulars: this.state.particulars.concat(this.state.firstName),
+      id: uniqid()
+    })
+  }
+
+  addFirstName = (e) => {
+    e.preventDefault();
+    return (
+      <div>{this.state.firstName}</div>
+    )
+  };
+
+  render(){
+    const particulars = this.state.particulars;
+    return (
+      <div className="App">
+        <div class = "cv-form">
+          <form id = "personalDetails" >
+            <h2>Personal Details</h2>
+            <label htmlFor="nameInput"></label>
+            <input type = "text" id="nameInput" value={this.state.name} onChange = {this.handleChange} name="name"/>
+            <label htmlFor="title"></label>
+            <input type = "text" id="title"  value={this.state.title} onChange = {this.handleChange}/>
+            <label htmlFor="phone"></label>
+            <input type = "text" id="phone" name="phone"  value={this.state.phone} onChange = {this.handleChange}/>
+            <label htmlFor="email"></label>
+            <input type = "text" id="email" name="email" value={this.state.email} onChange = {this.handleChange}/>
+            <label htmlFor="location"></label>
+            <input type = "text" id="location" name="location" value={this.state.location} onChange = {this.handleChange}/>
+            <textarea name = "description" defaultValue ="This is a description" rows = "5" value={this.state.description} onChange = {this.handleChange}></textarea>
+            <div className = "buttonClass">
+              <button type = "button">Delete</button> 
+              <button type = "button">Add</button>
+            </div>
+          </form>
+
+          <form id = "workExperience">
+            <h2>Work Experience</h2>
+            <label htmlFor ="companyName"></label>
+            <input type = "text" id ="companyName"/>
+            <label htmlFor ="jobTitle"></label>
+            <input type = "text" id ="jobTitle"/>
+            <label htmlFor ="jobStartDate"></label>
+            <input type = "text" id ="jobStartDate"/>
+            <label htmlFor ="jobEndDate"></label>
+            <input type = "text" id ="jobEndDate"/>
+            <label htmlFor ="jobDescription"></label>
+            <input type = "text" id ="jobDescription"/>
+            <div className = "buttonClass">
+              <button type = "button">Delete</button> 
+              <button type = "button">Add</button>
+            </div>
+          </form>
+          
+          <form id = "education">
+            <h2>Education</h2>
+            <label htmlFor ="educationName"></label>
+            <input type = "text" id ="educationName"/>
+            <label htmlFor ="institution"></label>
+            <input type = "text" id ="institution"/>
+            <label htmlFor ="startDate"></label>
+            <input type = "text" id ="startDate"/>
+            <label htmlFor ="endDate"></label>
+            <input type = "text" id ="endDate"/>
+            <div className = "buttonClass">
+              <button type = "button">Delete</button> 
+              <button type = "button">Add</button>
+            </div>
+          </form>
+        </div>
+     
+
+        <div className = "cv-view">
+            
+        </div>
+
     </div>
-  );
+    );
+  }
+  
 }
 
 export default App;
---
+++ src/components/Particulars.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+const Particulars = (props) =>{
+
+    const particulars = props;
+        
+    return(
+        <div>test</div>
+    )
+    
+}
+
+export default Particulars
